2556a4af5877142f7e301de872b0f5e80d314859
Christopher Ha <z5418665@ad.unsw.edu.au>
2023-06-08 09:55:28 +1000

    Added adminQuizCreate function prototype

---
+++ src/quiz.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} authUserId - passes through authUserId
+ * @param {number} email - passes through the email of the user
+ * @param {number} password - passes through the password of the user 
+ * @returns {quizId: 2} - returns quizId: 2
+ */
+function adminQuizCreate(authUserId, name, description) {
+        return {
+            quizId: 2
+        }
+    }
