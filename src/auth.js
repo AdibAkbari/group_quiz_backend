134b5399c4e46b0143957914cbe1a87ea2c3b7a7
Christopher Ha <z5418665@ad.unsw.edu.au>
2023-06-08 09:47:42 +1000

    Completed adminAuthLogin function

---
+++ src/auth.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} email - passes through the email of the user
+ * @param {number} password - passes through the password of the user 
+ * @returns {authUserId: 1} - returns authUserId: 1
+ */
+function adminAuthLogin(email, password) {
+    return {
+      authUserId: 1,
+    }
+}
+  
