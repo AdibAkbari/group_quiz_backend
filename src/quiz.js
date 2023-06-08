c534871822d1608e9b5d6757cfd65a124e3f4289
Christopher Ha <z5418665@ad.unsw.edu.au>
2023-06-08 13:07:30 +1000

    fixed up JScode

--- src/quiz.js
+++ src/quiz.js
@@ -1,7 +1,7 @@
 /**
  * @param {number} authUserId - passes through authUserId
- * @param {number} email - passes through the email of the user
- * @param {number} password - passes through the password of the user 
+ * @param {string} name - passes through the name of the user
+ * @param {string} description - passes through the description
  * @returns {quizId: 2} - returns quizId: 2
  */
 function adminQuizCreate(authUserId, name, description) {
