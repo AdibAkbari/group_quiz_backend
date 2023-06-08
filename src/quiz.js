432d4642fdcd0d3f2b0e3db0d7107255210ab2c8
Adib Akbari <z5488768@ad.unsw.edu.au>
2023-06-08 13:02:55 +1000

    stubbed adminQuizNameUpdate function

---
+++ src/quiz.js
@@ -0,0 +1,11 @@
+/**
+ * Update the name of the relevant quiz.
+ * 
+ * @param {number} authUserId - passes through authUserId
+ * @param {number} quizId - passes through the quizId to update the name of
+ * @param {number} name - passes through the name to update with
+ * @returns { } - empty object
+ */
+function adminQuizNameUpdate(authUserId, quizId, name) {
+    return { }
+}
