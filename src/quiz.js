6a4636adaae98c67bd55ea4ae59e40da500a17ae
Adib Akbari <z5488768@ad.unsw.edu.au>
2023-06-08 12:57:17 +1000

    Stubbed adminQuizRemove function

---
+++ src/quiz.js
@@ -0,0 +1,11 @@
+/**
+ * Given a particular quiz, permanently remove the quiz.
+ * 
+ * @param {number} authUserId - passes through authUserId
+ * @param {number} quizId - passes through the quizId of the quiz to remove
+ * @returns {} - empty object
+ */
+function adminQuizRemove(authUserId, quizId) {
+    return {
+    }
+}
