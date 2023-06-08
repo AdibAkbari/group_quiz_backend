47210cb86c156709c12d872599726c3cd788bd14
Christopher Ha <z5418665@ad.unsw.edu.au>
2023-06-08 13:25:35 +1000

    fixed up jscode

--- src/auth.js
+++ src/auth.js
@@ -1,3 +1,4 @@
+ /**
  * Register a user with an email, password, and names, then returns their authUserId value.
  * @param = {string} email
  * @param = {string} password
@@ -5,7 +6,6 @@
  * @param = {string} nameLast
  * @returns = {authUserId: 1} authuserId
  */
-
 function adminAuthRegister (email, password, nameFirst, nameLast) {
     return {
         authUserId: 1,
