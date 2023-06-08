1fc87a31c8c5b565167a3f053a7ef056d07c20c3
Christopher Ha <z5418665@ad.unsw.edu.au>
2023-06-08 10:01:46 +1000

    Completed adminQuizInfo function prototype

---
+++ src/quiz.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} quizId - passes through the quizId 
+ * @returns {
+ *           quizId: 1,
+ *           name: 'My Quiz',
+ *           timeCreated: 1683125870,
+ *           timeLastEdited: 1683125871,
+ *           description: 'This is my quiz',
+ *          } - returns Quiz info 
+ */
+function adminQuizInfo(authUserId, quizId) {
+    return {
+        quizId: 1,
+        name: 'My Quiz',
+        timeCreated: 1683125870,
+        timeLastEdited: 1683125871,
+        description: 'This is my quiz',
+    }
+}
