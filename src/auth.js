7b66d24e42c634db90e7cf3e297797e73821d8eb
Christopher Ha <z5418665@ad.unsw.edu.au>
2023-06-08 13:20:23 +1000

    added function description

--- src/auth.js
+++ src/auth.js
@@ -1,4 +1,5 @@
 /**
+ * Given a registered user's email and password returns their authUserId value.
  * @param {string} email - passes through the email of the user
  * @param {string} password - passes through the password of the user 
  * @returns {authUserId: 1} - returns authUserId: 1
