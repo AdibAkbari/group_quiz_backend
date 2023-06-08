58bf82f7f110e526d382d47e65f512bcfca8499f
Christopher Ha <z5418665@ad.unsw.edu.au>
2023-06-08 13:33:43 +1000

    added function description

--- src/quiz.js
+++ src/quiz.js
@@ -1,4 +1,5 @@
 /**
+ * Given basic details about a new quiz, create one for the logged in user.
  * @param {number} authUserId - passes through authUserId
  * @param {string} name - passes through the name of the user
  * @param {string} description - passes through the description
