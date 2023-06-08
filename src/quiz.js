1f92e7d21ed30f315f15c4972b4bbe8bf2c840d6
Darcy Faulkes <z5418261@ad.unsw.edu.au>
2023-06-08 13:00:53 +1000

    added a function to quiz.js

---
+++ src/quiz.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} authUserId - passes through authors user ID
+ * @param {number} quizId - passes through the id number for the quiz
+ * @param {string} description - passes through description of quiz
+ * @returns {} - doesn't return anything
+ */
+
+function adminQuizDescriptionUpdate (authUserID, quizId, description) {
+    
+    return { }
+
+}
