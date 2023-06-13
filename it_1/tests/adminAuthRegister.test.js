485b3a525d228e90703c2815aa6b6e7ad75d5f56
Sarah <z5310523@ad.unsw.edu.au>
2023-06-13 19:43:48 +1000

    started writing first test for adminAuthRegister

--- it_1/tests/adminAuthRegister.test.js
+++ it_1/tests/adminAuthRegister.test.js
@@ -1,1 +1,9 @@
+// tests for adminAuthRegister function
+import { adminAuthRegister } from './auth.js'
 
+describe ('test for adminAuthRegister', () => {
+    test.each([
+        {name: 'adminAuthRegister valid input', input: ('email@gmail.com', 'password','firstName', 'lastName'), output: 1 }
+    ])
+
+})
