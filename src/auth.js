6164e4626908135be596f147d6775f7b265780f2
Christopher Ha <z5418665@ad.unsw.edu.au>
2023-06-15 08:48:46 +1000

    fixed up JScode for auth.js stub functions

--- src/auth.js
+++ src/auth.js
@@ -1,10 +1,11 @@
- /**
- * Register a user with an email, password, and names, then returns their authUserId value.
- * @param = {string} email
- * @param = {string} password
- * @param = {string} nameFirst
- * @param = {string} nameLast
- * @returns = {authUserId: 1} authuserId
+/**
+ * Register a user with an email, password, and names, then returns their 
+ * authUserId value.
+ * @param {string} email 
+ * @param {string} password 
+ * @param {string} nameFirst 
+ * @param {string} nameLast 
+ * @returns {authUserId: 1} authuserId
  */
 function adminAuthRegister (email, password, nameFirst, nameLast) {
     return {
@@ -27,13 +28,12 @@ function adminAuthLogin(email, password) {
 /**
  * Given an admin user's authUserId, return details about the user.
  * "name" is the first and last name concatenated with a single space between them
- * @param = {number} authUserId
- * @returns = {{user: {userId: number, name: string, email: string, 
+ * @param {number} authUserId
+ * @returns {{user: {userId: number, name: string, email: string, 
  *                     numSuccessfulLogins: number,
  *                     numFailedPasswordsSinceLastLogin: number}}} object
  */
-
-function adminUserDetails (authUserId) {
+function adminUserDetails(authUserId) {
     return { 
         user: 
         {
