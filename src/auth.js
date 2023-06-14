7c3a14772475eebf61dc3842477cb1219c6d005a
Christopher Ha <z5418665@ad.unsw.edu.au>
2023-06-15 09:07:37 +1000

    fixed up JScode for auth.js and quiz.js stub functions

--- src/auth.js
+++ src/auth.js
@@ -1,6 +1,7 @@
 /**
  * Register a user with an email, password, and names, then returns their 
  * authUserId value.
+ * 
  * @param {string} email 
  * @param {string} password 
  * @param {string} nameFirst 
@@ -15,6 +16,7 @@ function adminAuthRegister (email, password, nameFirst, nameLast) {
 
 /**
  * Given a registered user's email and password returns their authUserId value.
+ * 
  * @param {string} email - passes through the email of the user
  * @param {string} password - passes through the password of the user 
  * @returns {authUserId: 1} - returns authUserId: 1
@@ -28,6 +30,7 @@ function adminAuthLogin(email, password) {
 /**
  * Given an admin user's authUserId, return details about the user.
  * "name" is the first and last name concatenated with a single space between them
+ * 
  * @param {number} authUserId
  * @returns {{user: {userId: number, name: string, email: string, 
  *                     numSuccessfulLogins: number,
