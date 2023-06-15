a3bfa820aaa30faf40b08f7c0ab0184f1c90e155
Sarah <z5310523@ad.unsw.edu.au>
2023-06-15 13:29:34 +1000

    added edge case tests to adminAuthRegister.test

--- it_1/tests/adminAuthRegister.test.js
+++ it_1/tests/adminAuthRegister.test.js
@@ -9,6 +9,41 @@ describe ('test for adminAuthRegister', () => {
             output: 1 
         },
         {
+            name: 'adminAuthRegister: valid input 2',
+            input: ('email3@hotmail.com', 'passw123','na', 'na'),
+            output: 1 
+        },
+        {
+            name: 'adminAuthRegister: valid input 3',
+            input: ('email4@yahoo.com', 'longPassword12345','nameIsExactlyTwentyy', 'lastIsExactlyTwentyy'),
+            output: 1 
+        },
+        {
+            name: 'adminAuthRegister: valid input 4',
+            input: ('email5@gmail.com', 'password1','name-First\'s', 'name-Last\'s'),
+            output: 1 
+        },
+        {
+            name: 'adminAuthRegister: empty email',
+            input: ('', 'password1','nameFirst', 'nameLast'),
+            output: {error: 'Email is invalid'} 
+        },
+        {
+            name: 'adminAuthRegister: empty nameFirst',
+            input: ('email2@gmail.com', 'password1','', 'nameLast'),
+            output: {error: 'First name must be between 2 and 20 characters'}
+        },
+        {
+            name: 'adminAuthRegister: empty nameLast',
+            input: ('email2@gmail.com', 'password1','nameFirst', ''),
+            output: {error: 'Last name must be between 2 and 20 characters'}
+        },
+        {
+            name: 'adminAuthRegister: empty password',
+            input: ('email2@gmail.com', '','nameFirst', ''),
+            output: {error: 'Password must be at least 8 characters'}
+        },
+        {
             name: 'adminAuthRegister: email already used',
             input: ('email@gmail.com', 'password1', 'nameFirst', 'nameLast'),
             output: {error: 'Email address is used by another user'}
@@ -21,6 +56,11 @@ describe ('test for adminAuthRegister', () => {
         {
             name: 'adminAuthRegister: invalid nameFirst',
             input: ('email2@gmail.com', 'password1', 'nameFirst$', 'nameLast'),
+            output: {error: 'Invalid first name'}
+        },
+        {
+            name: 'adminAuthRegister: invalid nameFirst 2',
+            input: ('email2@gmail.com', 'password1', 'nameFirst$().', 'nameLast'),
             output: {error: 'Invalid first name'}
         },
         {
@@ -39,6 +79,11 @@ describe ('test for adminAuthRegister', () => {
             output: {error: 'Invalid last name'}
         },
         {
+            name: 'adminAuthRegister: invalid nameLast 2',
+            input: ('email2@gmail.com', 'password1', 'nameFirst', 'nameLast1'),
+            output: {error: 'Invalid last name'}
+        },
+        {
             name: 'adminAuthRegister: nameLast too short',
             input: ('email2@gmail.com', 'password1', 'nameLast', 'n'),
             output: {error: 'Last name must be between 2 and 20 characters'}
@@ -54,10 +99,15 @@ describe ('test for adminAuthRegister', () => {
             output: {error: 'Password must be at least 8 characters'}
         },
         {
-            name: 'adminAuthRegister: password must contain number and letter',
+            name: 'adminAuthRegister: password must contain number',
             input: ('email2@gmail.com', 'password', 'nameFirst', 'nameLast'),
             output: {error: 'Password must contain at least one number and one letter'}
         },
+        {
+            name: 'adminAuthRegister: password must contain letter',
+            input: ('email2@gmail.com', '12345678', 'nameFirst', 'nameLast'),
+            output: {error: 'Password must contain at least one number and one letter'} 
+        }
     ])('adminAuthRegister edge case testing', ({input,output}) => {
         expect(adminAuthRegister(input)).toBe(output);
     });
