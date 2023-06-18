422ae4a2e0ed78fd44c744d4f68c721f1185419a
Darcy Faulkes <z5418261@ad.unsw.edu.au>
2023-06-18 23:02:58 +1000

    wrote function adminUserDetails

--- src/auth.js
+++ src/auth.js
@@ -1,3 +1,5 @@
+import {setData, getData} from './dataStore.js'
+
 /**
  * Register a user with an email, password, and names, then returns their 
  * authUserId value.
@@ -37,14 +39,65 @@ export function adminAuthLogin(email, password) {
  *                     numFailedPasswordsSinceLastLogin: number}}} object
  */
 export function adminUserDetails(authUserId) {
+    let data = getData();
+    
+    if (!isValidUserId(authUserId)) {
+        return {
+            error: 'AuthUserId is not a valid user'
+        }
+    };
+
+    let i = findUserIndex(authUserId);
+    if (i === -1) {
+        return {
+            error: 'AuthUserId is not a valid user'
+        }
+    }
+    let name = `${data.users[i].nameFirst} ${data.users[i].nameLast}`;
+
     return { 
         user: 
         {
-            userId: 1,
-            name: 'Hayden Smith',
-            email: 'hayden.smith@unsw.edu.au',
-            numSuccessfulLogins: 3,
-            numFailedPasswordsSinceLastLogin: 1,
+            userId: data.users[i].authUserId,
+            name: name,
+            email: data.users[i].email,
+            numSuccessfulLogins: data.users[i].numSuccessfulLogins,
+            numFailedPasswordsSinceLastLogin: data.users[i].numFailedPasswordsSinceLastLogin,
         }
     }
+}
+
+/**
+ * Checks whether a given number is a valid user id
+ * 
+ * @param {number} authUserId 
+ * @returns {boolean} true if valid, false if invalid
+ */
+function isValidUserId(authUserId) {
+    if(isNaN(authUserId)) {
+        return false;
+    };
+    let data = getData();
+    for (const current of data.users) {
+        if (current.authUserId === authUserId) {
+            return true;
+        }
+    };
+    return false;
+}
+
+/**
+ * finds the array index of a given user id
+ * 
+ * @param {number} authUserId 
+ * @returns {number} index number that corresponds to user id
+ */
+function findUserIndex(authUserId) {
+    let data = getData();
+    for (const i in data.users) {
+        if (data.users[i].authUserId === authUserId) {
+            return i;
+        }
+    }
+    return -1;
 }
