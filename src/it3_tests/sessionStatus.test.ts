74aa8e44ee1563f9d26c5082b053dcd21f15f520
Christopher Ha <z5418665@ad.unsw.edu.au>
2023-08-01 18:19:39 +1000

    cleaned up test file session status

--- src/it3_tests/sessionStatus.test.ts
+++ src/it3_tests/sessionStatus.test.ts
@@ -14,14 +14,13 @@ import HTTPError from 'http-errors';
 let token: string;
 let quizId: number;
 let sessionId: number;
-// let questionId: number;
+
 const validAnswers = [{ answer: 'answer1', correct: true }, { answer: 'answer2', correct: false }];
 
 beforeEach(() => {
   clearRequest();
   token = authRegisterRequest('email@gmail.com', 'password1', 'first', 'last').body.token;
   quizId = quizCreateRequest(token, 'quiz1', '').quizId;
-  // questionId = createQuizQuestionRequest(quizId, token, 'Question 1', 5, 6, validAnswers).questionId;
   createQuizQuestionRequest(quizId, token, 'Question 1', 5, 6, validAnswers);
   sessionId = startSessionRequest(quizId, token, 3).sessionId;
 });
